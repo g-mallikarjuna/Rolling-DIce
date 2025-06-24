9f358b790ecf0d6704af8008c2cf2bec91b4d7cd
Arjun <mallikarjuna44g@gmail.com>
2025-06-24 13:10:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function rolldice() {
     for (let index = 0; index < numberroll; index++) {
         const numberValue = Math.floor(Math.random() * 6) + 1;
         values.push(numberValue);
-        images.push(`<img src="image/${numberValue}.svg" width=100">`)
+        images.push(`<img src="images/${numberValue}.svg" width=100">`)
     }
     displayResult.textContent = `Dice: ${values.join(", ")}`;
     displayImage.innerHTML = images.join(" ");
